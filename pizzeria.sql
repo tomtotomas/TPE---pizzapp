a1a0bc4320d36df0f5d872622d0481197127b0c7
tomtotomas <tomas.a.gomez.zb@gmail.com>
2024-09-16 19:40:53 -0300

    Add files via upload

---
+++ pizzeria.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-09-2024 a las 00:37:46
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pizzeria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'Clasica'),
+(2, 'Especial');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pizza`
+--
+
+CREATE TABLE `pizza` (
+  `pizza_id` int(11) NOT NULL,
+  `pizza_name` text NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `pizza`
+--
+
+INSERT INTO `pizza` (`pizza_id`, `pizza_name`, `category_id`) VALUES
+(1, 'Mozzarella', 1),
+(2, 'Fugazzeta', 1),
+(3, 'Jamon y queso', 2),
+(4, 'Napolitana', 2),
+(5, 'Capresse', 2),
+(6, 'Hawaiana', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `birthday` date NOT NULL,
+  `adress` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `name`, `email`, `password`, `birthday`, `adress`) VALUES
+(1, 'ellokodlaspizzas125', 'martin gimenez', 'el_lokotincho_goku@hotmail.com', '123456', '1998-02-24', 'Necochea 1254'),
+(2, 'sofi_piolaa', 'sofia valdez', 'sofi_casiangeles@hotmail.com.ar', 'localinda1245', '1993-05-14', 'Independencia 1798'),
+(3, 'pelukita', 'elias nuñez', 'elias_nuñez_s@hotmail.com', 'yugioh123', '1999-10-28', 'Dorrego 2350');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indices de la tabla `pizza`
+--
+ALTER TABLE `pizza`
+  ADD PRIMARY KEY (`pizza_id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pizza`
+--
+ALTER TABLE `pizza`
+  MODIFY `pizza_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
